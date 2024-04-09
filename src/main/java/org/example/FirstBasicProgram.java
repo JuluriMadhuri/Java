19fb08f121e678e4968dc27621844fb15eb4fb37
juluri madhuri <julurimadhuri0414@gmail.com>
2024-04-10 00:24:29 +0530

    First Project

---
+++ src/main/java/org/example/FirstBasicProgram.java
@@ -0,0 +1,12 @@
+package org.example;
+
+/**
+ * Hello world!
+ */
+public class FirstBasicProgram {
+    public static void main(String[] args) {
+        System.out.println("Hello Madhuri");
+        // System.out.println('hello madhuri'); - if it is only one character then we can close it in single quotes.
+        //  System.out.println("hello); - unclosed string literal
+    }
+}
