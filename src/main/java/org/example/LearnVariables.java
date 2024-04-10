22fc02a18ae5cc6bf86a4800bceeba2d951b9e24
juluri madhuri <julurimadhuri0414@gmail.com>
2024-04-10 17:59:01 +0530

    Variables concept

---
+++ src/main/java/org/example/LearnVariables.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class LearnVariables {
+    public static void main(String[] args) {
+        int myFirstNumber = 10;
+        System.out.println(myFirstNumber);
+        myFirstNumber = 20;
+        System.out.println(myFirstNumber);
+        int mySecondNumber = 45;
+        int myThirdNumber = 57;
+        int sum = myThirdNumber + myFirstNumber + mySecondNumber;
+        System.out.println(sum);
+    }
+}
